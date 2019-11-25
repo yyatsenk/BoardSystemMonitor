db6e77a0008c07732e5b735f4f544ff6046e5ff0
Yaroslav Yatsenko <yyatsenk@gmail.com>
2019-11-25 22:22:11 +0200

    Wifi scan approach in progress

--- app/src/main/java/com/volvo/BoardSystemMonitor/MainActivity.java
+++ app/src/main/java/com/volvo/BoardSystemMonitor/MainActivity.java
@@ -13,6 +13,7 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import  android.widget.TextView;
+import android.widget.Toast;
 
 import android.os.Bundle;
 
@@ -21,37 +22,82 @@ public class MainActivity extends AppCompatActivity {
     WifiManager wifi;
     List<ScanResult> results;
     int size = 0;
-
+    TextView text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final Button wifiDiscoverBtn = (Button) findViewById(R.id.wifiDiscoverBtn);
-        wifiDiscoverBtn.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                wifi.startScan();
-            }
-        });
-        wifi = (WifiManager) getSystemService(Context.WIFI_SERVICE); //getApplicationContext()
-        if (wifi.isWifiEnabled() == false)
-        {
-            System.out.println("wifi is disabled..making it enabled");
-            wifi.setWifiEnabled(true);
-        }
-        registerReceiver(new BroadcastReceiver()
-        {
+//        final Button wifiDiscoverBtn = (Button) findViewById(R.id.wifiDiscoverBtn);
+//        text = (TextView) findViewById(R.id.fountField);
+//        wifi = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+//        if (wifi.isWifiEnabled() == false)
+//        {
+//            System.out.println("wifi is disabled..making it enabled");
+//            wifi.setWifiEnabled(true);
+//        }
+//        registerReceiver(new BroadcastReceiver()
+//        {
+//            @Override
+//            public void onReceive(Context c, Intent intent)
+//            {
+//                results = wifi.getScanResults();
+//
+//                size = results.size();
+//                unregisterReceiver(this);
+//                Toast.makeText(getApplicationContext(), "I revieve " + size, Toast.LENGTH_SHORT).show();
+//            }
+//        }, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+//        wifi.startScan();
+//        wifiDiscoverBtn.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                Toast.makeText(getApplicationContext(), "Scanning....", Toast.LENGTH_SHORT).show();
+//                text.setText("debug");
+//                try {
+//                    text.setText("Size = " + size);
+//                } catch (Exception e) {
+//                    text.setText("Exeption happen!");
+//                }
+//            }
+//        });
+        wifi = (WifiManager)
+                getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+
+        BroadcastReceiver wifiScanReceiver = new BroadcastReceiver() {
             @Override
-            public void onReceive(Context c, Intent intent)
-            {
-                results = wifi.getScanResults();
-                size = results.size();
-                final TextView text = (TextView)findViewById(R.id.fountField);
-                text.setText(size + " wifi points found");
+            public void onReceive(Context c, Intent intent) {
+                boolean success = intent.getBooleanExtra(
+                        WifiManager.EXTRA_RESULTS_UPDATED, false);
+                if (success) {
+                    scanSuccess();
+                } else {
+                    // scan failure handling
+                    scanFailure();
+                }
             }
-        }, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+        };
+
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION);
+        getApplicationContext().registerReceiver(wifiScanReceiver, intentFilter);
+
+        boolean success = wifi.startScan();
+        if (!success) {
+            Toast.makeText(getApplicationContext(), "Failure!", Toast.LENGTH_SHORT).show();
+            // scan failure handling
+            scanFailure();
+        }
+        Toast.makeText(getApplicationContext(), "Success!", Toast.LENGTH_SHORT).show();
+        scanSuccess();
     }
-    protected void onClick(View view) {
-        ;
+    private void scanSuccess() {
+        results = wifi.getScanResults();
+        Toast.makeText(getApplicationContext(), "Size = " + results.size(), Toast.LENGTH_SHORT).show();
+    }
+
+    private void scanFailure() {
+        // handle failure: new scan did NOT succeed
+        // consider using old scan results: these are the OLD results!
+        results = wifi.getScanResults();
     }
 }
